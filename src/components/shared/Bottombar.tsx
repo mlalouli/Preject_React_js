e6c687e6439ff12a7da113c0de58996c278cef67
Meryem Lalouli <mlalouli@e1r3p15.1337.ma>
2024-01-31 00:55:16 +0100

    Create a page for create post

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -15,14 +15,16 @@ function Bottombar() {
             key={link.label} 
             className={` ${
               isActive && 'bg-primary-500 rounded-[10px]'
-            } flex-center`}
+            } flex-center flex-col gap-1 p-2 transition`}
           >
             <img
               src={link.imgURL}
               alt={link.label}
-              className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+              width={16}
+              height={16}
+              className={`${isActive && 'invert-white'}`}
             />
-            {link.label}
+            <p className="tiny-medium text-light-2">{link.label}</p>
           </Link>
         )
         })}
