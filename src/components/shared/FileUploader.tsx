f8342a23f41562752ac3e0e2d5616cc55a87a923
Meryem Lalouli <mlalouli@e1r2p12.1337.ma>
2024-01-31 23:40:08 +0100

    Craete a content the CreatePost

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,58 @@
+import React, {useCallback, useState} from 'react'
+import { FileWithPath, useDropzone} from 'react-dropzone'
+import { Button } from '../ui/button';
+
+type FileUploaderProps = {
+  fielsChange: (FILES: File[]) => void;
+  mediaUrl: string;
+}
+
+const FileUploader = ({ fielsChange, mediaUrl}: FileUploaderProps) => {
+
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState('');
+
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+    setFile(acceptedFiles);
+    fielsChange(acceptedFiles);
+    setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+  }, [file])
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      'image/*' : ['.png', '.jpeg', '.jpg', '.svg']
+    }
+  })
+  return (
+    <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+      <input {...getInputProps()} className='cusror-pointer' />
+      {
+        fileUrl ? (
+          <div>
+            test 1
+          </div>
+        ) : (
+          <div className='file_uploader-box'>
+            <img 
+              src='/assets/icons/file-upload.svg'
+              width={96}
+              height={77}
+              alt='file-upload'
+            />
+
+              <h3 className="base-medium text-light-2 mb-2 mt-6">Drag photo here</h3>
+            <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+
+            <Button className="shad-button_dark_4">
+              Select from computer
+            </Button>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default FileUploader
