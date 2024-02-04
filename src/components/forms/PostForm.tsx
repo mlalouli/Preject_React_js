ae9b12776df3ef8c1f9a0b1b57ae5b679a1d3c5a
Meryem Lalouli <mlalouli@e1r3p11.1337.ma>
2024-02-04 14:51:12 +0100

    Create Post and save it in database

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,42 +1,56 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
+import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
+import { toast, useToast } from "../ui/use-toast"
+import { useNavigate } from 'react-router-dom';
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+type PostFormProps = {
+  post?: Models.Document;
+}
 
-const PostForm = ({ post }) => {
+
+const PostForm = ({ post }: PostFormProps) => {
+    const { mutateAsync: createPost, isPending: isLoadingCreate } =
+      useCreatePost();
+
+      const { user } = useUserContext();
+      const { toast } = useToast();
+      const navigate = useNavigate();
 
     // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      username: "",
+      caption: post ? post?.caption : "",
+      file: [],
+      location: post ? post?.location : "",
+      tags: post ? post.tags.join(',') : ''
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    const newPost = await createPost({
+      ...values,
+      userId: user.id,
+    });
+
+    if (!newPost) {
+      toast({
+        title: 'Please try again',
+      })
+    }
+    navigate('/');
   }
 
   return (
@@ -66,7 +80,6 @@ const PostForm = ({ post }) => {
                   fieldChange={field.onChange}
                   mediaUrl={post?.imageUrl}
                 />
-                <FileUploader />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -79,7 +92,7 @@ const PostForm = ({ post }) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" />
+                <Input type="text" className="shad-input" {...field} />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -97,6 +110,7 @@ const PostForm = ({ post }) => {
                 type="text" 
                 className="shad-input"
                 placeholder="JS, React, NextJS"
+                {...field}
                 />
               </FormControl>
               <FormMessage className="shad-form_message" />
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,9 @@ import{
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -22,5 +23,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
       mutationFn: signOutAccount,
+    });
+  };
+
+  export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (post: INewPost) => createPost(post),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+      },
     });
   };
